80cfc56c1061a12ccfc0ea32634403cbab271ef6
Kreecha Puphaiboon <puphaiboon.kreecha@gmail.com>
2025-08-29 15:32:06 +0700

    add copyright

--- src/sisr_alns_pdptw.py
+++ src/sisr_alns_pdptw.py
@@ -1,4 +1,26 @@
+#-*- coding: utf-8 -*-
 """
+Created on Thur August 14 04:26:17 2025
+
+ALNS (Adaptive Large Neighborhood Search) implementation for PDPTW
+Using the ALNS library https://github.com/N-Wouda/ALNS/tree/master 
+to solve Li-Lim benchmark instances download files from https://www.sintef.no/projectweb/top/pdptw/li-lim-benchmark/
+
+Data instances The format of the data files is as follows:
+
+The first line gives the number of vehicles, the capacity, and the speed (not used)
+From the second line, for each customer (starting with the depot):
+The index of the customer
+The x coordinate
+The y coordinate
+The demand
+The earliest arrival
+The latest arrival
+The service time
+The index of the corresponding pickup order (0 if the order is a pickup)
+The index of the corresponding delivery order (0 if the order is a delivery)
+
+
 SISR-inspired ALNS Operators for PDPTW (Pickup and Delivery Problem with Time Windows)
 Adapted from Slack Induction String Removal (SISR) for the N-Wouda ALNS framework
 
@@ -7,6 +29,30 @@ This converts SISR logic for PDPTW problems where:
 - Pickup must precede its delivery (precedence constraint)
 - Routes contain mixed pickup/delivery sequences
 - Time windows and capacity constraints apply
+s
+@author: Kreecha_P
+
+MIT License
+
+Copyright (c) 2025 Kreecha Puphaiboon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 import os
 
